8e243df1765c9368283f08624f91780501db11b7
Erfath <erfathparvez1@gmail.com.com>
2022-04-18 14:14:55 +0600

    Signin and Signout part done

--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -1,9 +1,18 @@
+import { signOut } from 'firebase/auth';
 import React from 'react';
 import { Container, Nav, Navbar} from 'react-bootstrap';
+import { useAuthState } from 'react-firebase-hooks/auth';
 import { Link } from 'react-router-dom';
+import auth from '../../../firebase.init';
 import logo from '../../../images/Logo/logo1.png'
 
 const Header = () => {
+    const [user] = useAuthState(auth);
+
+    const handleSignout = () =>{
+        signOut(auth);
+    }
+
     return (
         <Navbar sticky='top' collapseOnSelect expand="lg" bg="primary" variant="dark">
             <Container>
@@ -18,9 +27,13 @@ const Header = () => {
                         <Nav.Link href="home#services" className='text-white me-3 fs-4'>My Services</Nav.Link>
                         <Nav.Link href="home#blogs" className='text-white me-3 fs-4'>Blogs</Nav.Link>
                         <Nav.Link as={Link} to="contact" className='text-white me-3 fs-4'>About Me</Nav.Link>
-                        <Nav.Link as={Link} to="login" className='text-white fs-4'>
+                        {
+                            user ?
+                                <button className='btn btn-primary fs-4' onClick={handleSignout}>Sign out</button>
+                            :
+                            <Nav.Link as={Link} to="login" className='text-white fs-4'>
                             Login
-                        </Nav.Link>
+                        </Nav.Link>}
                     </Nav>
                 </Navbar.Collapse>
             </Container>
